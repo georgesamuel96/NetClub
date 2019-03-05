d9b47ba8872d9be21e3d07c4162771a552b8f37a
georgesamuel96 <georges012441@gmail.com>
2019-03-05 02:46:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/georgesamuel/netclub/RegisterFirstPageActivity.java
@@ -0,0 +1,13 @@
+package com.example.georgesamuel.netclub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterFirstPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_first_page);
+    }
+}
