72ca80ec5044f8795eb2ee4ce6de01269be856c3
georgesamuel96 <georges012441@gmail.com>
2019-03-07 22:22:36 +0200

    Email Verify Commit

--- app/src/main/java/com/egcoders/technologysolutions/netclub/RegisterFirstPageActivity.java
+++ app/src/main/java/com/egcoders/technologysolutions/netclub/RegisterFirstPageActivity.java
@@ -3,11 +3,18 @@ package com.egcoders.technologysolutions.netclub;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterFirstPageActivity extends AppCompatActivity {
 
@@ -16,6 +23,7 @@ public class RegisterFirstPageActivity extends AppCompatActivity {
     private EditText confirmPasswordText;
     private ImageView nextImage;
     private AlertDialog.Builder alertBuilder;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,22 +37,60 @@ public class RegisterFirstPageActivity extends AppCompatActivity {
         confirmPasswordText = (EditText) findViewById(R.id.confirmPass);
         nextImage = (ImageView) findViewById(R.id.next);
 
+        mAuth = FirebaseAuth.getInstance();
+
         nextImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String email = emailText.getText().toString().trim();
-                String password = passwordText.getText().toString().trim();
+                final String email = emailText.getText().toString().trim();
+                final String password = passwordText.getText().toString().trim();
                 String confirmPass = confirmPasswordText.getText().toString().trim();
 
                 if(!missingValue(email, password, confirmPass)){
 
                     if(password.equals(confirmPass)) {
-                        Intent i = new Intent(RegisterFirstPageActivity.this, RegisterSecondPageActivity.class);
-                        i.putExtra("email", email);
-                        i.putExtra("password", password);
-                        startActivity(i);
-                        finish();
+
+                        mAuth.signInAnonymously().addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+
+                                    mAuth.getCurrentUser().updateEmail(email);
+                                    mAuth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                Intent i = new Intent(RegisterFirstPageActivity.this, RegisterSecondPageActivity.class);
+                                                i.putExtra("email", email);
+                                                i.putExtra("password", password);
+                                                startActivity(i);
+                                                finish();                                            }
+                                            else{
+                                                alertBuilder.setTitle("Email Error");
+                                                alertBuilder.setMessage("You should inter valid email");
+                                                alertBuilder.setCancelable(false);
+                                                alertBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(DialogInterface dialog, int which) {
+                                                        passwordText.setText("");
+                                                        confirmPasswordText.setText("");
+                                                        dialog.cancel();
+                                                    }
+                                                });
+                                                AlertDialog alertDialog = alertBuilder.create();
+                                                alertDialog.show();
+                                            }
+                                        }
+                                    });
+
+                                }
+                                else {
+
+                                }
+                            }
+                        });
+
                     }
                     else{
                         alertBuilder.setTitle("Authentication Error");
--- app/src/main/java/com/egcoders/technologysolutions/netclub/RegisterSecondPageActivity.java
+++ app/src/main/java/com/egcoders/technologysolutions/netclub/RegisterSecondPageActivity.java
@@ -17,6 +17,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
